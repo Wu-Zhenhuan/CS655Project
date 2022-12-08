4445089e83a9cc3b53cdbc6ad5ae39e16db2d426
Zhenhuan Wu <zhenhuan@bu.edu>
2022-12-08 18:19:55 -0500

    Manager script (rough draft)

---
+++ Manager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Manager Setup Script"
+echo "Installing Java..."
+sudo apt-get update
+sudo apt-get --yes install default-jdk
+echo "Cloning GitHub repo to current directory..."
+git clone https://github.com/Wu-Zhenhuan/CS655Project.git
+echo "Compiling..."
+cd CS655Project
+cd src
+javac Manager.java
+echo "Launching Manager..."
+java Manager.class
